6326593b60d5f1f37fb3ca087a2d90ba405bdba0
kagklis <kagklis@gmail.com>
2022-07-09 16:25:16 +0300

    Used NgControl injection instead.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+/**
+ * Based on Kara Erickson's presentation: https://www.youtube.com/watch?v=CD_t3m2WMM8
+ * You can also check my Medium article on custom form controls and nested form groups:
+ * https://javascript.plainenglish.io/angular-custom-form-controls-nested-form-groups-made-easy-2ac09e91cf67
+ */
 
 @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-   form!: FormGroup;
+  @ViewChild('form') form!: NgForm;
 
-   constructor(private fb: FormBuilder) {}
+  constructor() {}
 
-   ngOnInit(): void {
-      this.form = this.fb.group({
-         nickname: ['', Validators.required],
-         personal: ['']
-      });
-   }
+  ngOnInit(): void {}
 
-   onSubmit(): void {
-     console.log(this.form.value);
-   }
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
--- src/app/shared/personal/personal.component.ts
+++ src/app/shared/personal/personal.component.ts
@@ -1,99 +1,90 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
-   ControlValueAccessor,
-   FormGroup,
-   Validators,
-   Validator,
-   AbstractControl,
-   ValidationErrors,
-   NG_VALUE_ACCESSOR,
-   FormBuilder,
-   NG_VALIDATORS,
+  ControlValueAccessor,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  FormBuilder,
+  NgControl,
 } from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
-   selector: 'app-personal',
-   templateUrl: './personal.component.html',
-   styleUrls: ['./personal.component.scss'],
-   providers: [
-      {
-         provide: NG_VALUE_ACCESSOR,
-         multi: true,
-         useExisting: PersonalComponent,
-      },
-      {
-         provide: NG_VALIDATORS,
-         multi: true,
-         useExisting: PersonalComponent,
-      },
-   ],
+  selector: 'app-personal',
+  templateUrl: './personal.component.html',
+  styleUrls: ['./personal.component.scss'],
 })
 export class PersonalComponent
-   implements OnInit, OnDestroy, ControlValueAccessor, Validator
+  implements OnInit, OnDestroy, ControlValueAccessor
 {
-   form!: FormGroup;
+  form: FormGroup = this.fb.group({
+    first: ['', Validators.required],
+    last: ['', Validators.required],
+    gender: ['', Validators.required],
+    genderOther: ['', Validators.required],
+  });
 
-   genderOptions: string[] = [
-      'Female',
-      'Male',
-      'Non-binary',
-      'Gender non-conforming',
-      'Prefer not to say',
-      'Other',
-   ];
+  genderOptions: string[] = [
+    'Female',
+    'Male',
+    'Non-binary',
+    'Gender non-conforming',
+    'Prefer not to say',
+    'Other',
+  ];
 
-   onTouched: () => void = () => {};
-   onChange: (value: any) => void = () => {};
-   subscriptions: Subscription;
+  onTouched: () => void = () => {};
+  onChange: (value: any) => void = () => {};
+  subscriptions: Subscription;
 
-   constructor(private fb: FormBuilder) {
-      this.subscriptions = new Subscription();
-   }
+  constructor(private fb: FormBuilder, private ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+    this.subscriptions = new Subscription();
+  }
 
-   ngOnInit(): void {
-      this.form = this.fb.group(
-         {
-            first: ['', Validators.required],
-            last: ['', Validators.required],
-            gender: ['', Validators.required],
-            genderOther: ['', Validators.required],
-         },
-         { validators: this.allRequiredFieldsFilled }
-      );
-   }
+  ngOnInit(): void {
+    const control = this.ngControl.control;
+    if (!control) return;
+    const validators = control.validator
+      ? [control.validator, this.allRequiredFieldsFilled]
+      : this.allRequiredFieldsFilled;
+    control.addValidators(validators);
+    control.updateValueAndValidity();
+  }
 
-   ngOnDestroy(): void {
-      this.subscriptions.unsubscribe();
-   }
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
-   validate(control: AbstractControl): ValidationErrors | null {
-      return this.allRequiredFieldsFilled(control);
-   }
+  allRequiredFieldsFilled(control: AbstractControl): ValidationErrors | null {
+    const controlValue = control.value;
+    if (!controlValue) return { required: true };
+    const isValid =
+      controlValue.first &&
+      controlValue.last &&
+      controlValue.gender &&
+      (controlValue.gender !== 'Other' || controlValue.genderOther);
+    return isValid ? null : { required: true };
+  }
 
-   allRequiredFieldsFilled(control: AbstractControl): ValidationErrors | null {
-      const controlValue = control.value;
-      const isValid =
-         controlValue.first &&
-         controlValue.last &&
-         controlValue.gender &&
-         (controlValue.gender !== 'Other' || controlValue.genderOther);
-      return isValid ? null : { required: true };
-   }
+  onGenderChange(): void {
+    this.form?.get('genderOther')?.reset('');
+  }
 
-   writeValue(value: any): void {
-      value && this.form.setValue(value, { emitEvent: false });
-   }
+  writeValue(value: any): void {
+    value && this.form.setValue(value, { emitEvent: false });
+  }
 
-   registerOnChange(onChange: (value: any) => void): void {
-      this.subscriptions.add(this.form.valueChanges.subscribe(onChange));
-   }
+  registerOnChange(onChange: (value: any) => void): void {
+    this.subscriptions.add(this.form.valueChanges.subscribe(onChange));
+  }
 
-   registerOnTouched(onTouched: () => void): void {
-      this.onTouched = onTouched;
-   }
+  registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
+  }
 
-   setDisabledState(disabled: boolean): void {
-      disabled ? this.form.disable() : this.form.enable();
-   }
+  setDisabledState(disabled: boolean): void {
+    disabled ? this.form.disable() : this.form.enable();
+  }
 }
